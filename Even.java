f47c933f056284fd42d36d7feeb8c87d3a281ed9
srinivasaraothuvvapati <41735425+srinivasaraothuvvapati@users.noreply.github.com>
2018-07-27 10:24:17 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s = new Scanner (System.in);
+	     int srinu = s.nextInt();
+      if (srinu > 0)
+       {
+			if ( srinu % 2 == 0)
+	         {
+	         	System.out.println("Even");
+	         }	 
+	         		else 
+	         {
+	         	System.out.println("Odd");
+	         }	        
+          }
+                if (srinu < 0)
+	         {
+	         	System.out.println("invalid");
+	         }	       
+	}
+}
+	
+	
+	
