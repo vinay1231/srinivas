5f8cce71758aa721ae6452f52835b399daff8b44
srinivasaraothuvvapati <41735425+srinivasaraothuvvapati@users.noreply.github.com>
2018-07-27 09:24:37 +0530

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner (System.in);
+		int a = s.nextInt();
+		   if (a > 0)
+		    {
+		    	System.out.println("Positive");
+		    }
+		   else if (a == 0)
+		    {
+		    	System.out.println("Zero");
+		    }
+		    	else
+		    	 {
+		    	 	System.out.println("Negative");
+		    	 }
+	}
