8da3e8b2bba8b7dd9fb3031427d6e663be9f27e2
srinivasaraothuvvapati <41735425+srinivasaraothuvvapati@users.noreply.github.com>
2018-08-02 13:43:15 +0530

    Create 14th.java

---
+++ 14th.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int num1 = s.nextInt();
+		int num2 = s.nextInt();
+		for (int i = num1+1; i < num2; i++) {
+			if (i % 2 != 0) {
+				System.out.print(i + " ");
+			}
+		}
+	}
+}
