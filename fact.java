43c69fadf4ae072b127388426927afdf1911a9d4
srinivasaraothuvvapati <41735425+srinivasaraothuvvapati@users.noreply.github.com>
2018-08-07 22:06:00 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+		 Scanner s = new Scanner (System.in);
+
+		int h = s.nextInt();
+
+		int m = 1;
+
+		 for (int i =1; i<= h; i++)
+
+		   m = m*i;
+
+		   System.out.println(m);
+
+	}
+
+}
