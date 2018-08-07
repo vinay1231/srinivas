f0291b67e9a84af7c0b7d5da7d40daf16c6af19d
srinivasaraothuvvapati <41735425+srinivasaraothuvvapati@users.noreply.github.com>
2018-08-07 22:09:53 +0530

    Create multiple.java

---
+++ multiple.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+		 Scanner s = new Scanner (System.in);
+
+		int m = s.nextInt();
+
+		int h = 5,d;
+
+		 for (int i =1; i<= h; i++)
+
+		   {
+
+		   	d = m*i;
+
+		   System.out.println(d);
+
+		   }
+
+	}
+
+}
