81f05405b1a37e28f993a53c4b002a4b6f979d63
srinivasaraothuvvapati <41735425+srinivasaraothuvvapati@users.noreply.github.com>
2018-08-10 12:03:30 +0530

    Create createtime.java

---
+++ createtime.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+
+/* Name of the class has to be "Main" only if the class is public. */
+
+class Ideone
+
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+	     Scanner s = new Scanner (System.in);
+
+	     int t = s.nextInt();
+
+	      int k = (t/60); 
+
+	      int z = (t%60);
+
+	      System.out.println(k+" "+z);
+
+	}
+
+}
