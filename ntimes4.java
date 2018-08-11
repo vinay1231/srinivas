b31403511dcdcb8f87e759157e90d1ec589a55a5
srinivasaraothuvvapati <41735425+srinivasaraothuvvapati@users.noreply.github.com>
2018-08-11 12:42:59 +0530

    Create ntimes4.java

---
+++ ntimes4.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Ideone
+
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+	   Scanner s = new Scanner (System.in);
+
+	   String str = s.next();
+
+	    int k = s.nextInt();
+
+	    for (int i=1;i<=k;i++)
+
+	    {
+
+	    	System.out.println(str);
+
+	    }
+
+	}
+
+}
