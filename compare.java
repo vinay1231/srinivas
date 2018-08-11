32ce6a3df8c8da6d5e631a8ecfb9290a652ca2dd
srinivasaraothuvvapati <41735425+srinivasaraothuvvapati@users.noreply.github.com>
2018-08-11 12:21:37 +0530

    Create compare.java

---
+++ compare.java
@@ -0,0 +1,84 @@
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+
+/* Name of the class has to be "Main" only if the class is public. */
+
+class Ideone
+
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+	     Scanner s = new Scanner (System.in);
+
+	    int a = s.nextInt();
+
+	    int b = s.nextInt(); 
+
+	    int c = s.nextInt();
+
+	    int d = s.nextInt(); 
+
+	    int e = s.nextInt();
+
+	    int f = s.nextInt();
+
+	    int g = s.nextInt();
+
+	    int h = s.nextInt();
+
+	    int i = s.nextInt();
+
+	    int j = s.nextInt();
+
+	    if (a>b && a>c && a>d &&a>e &&a>f &&a>g &&a>h &&a>i &&a>j)
+
+	        System.out.println(a);
+
+	    else if (b>a && b>c && b>d &&b>e &&b>f &&b>g &&b>h &&b>i &&b>j)
+
+	     System.out.println(b);
+
+	    else if (c>a && c>b && c>d &&c>e &&c>f &&c>g &&c>h &&c>i &&c>j)
+
+	     System.out.println(c);
+
+	     else if (d>a && d>b && d>c &&d>e &&d>f &&d>g &&d>h &&d>i &&d>j)
+
+	     System.out.println(d);
+
+	     else if (e>a && e>b && e>c &&e>d &&e>f &&e>g &&e>h &&e>i &&e>j)
+
+	     System.out.println(e);
+
+	     else if (f>a && f>b && f>c &&f>d &&f>e &&f>g &&f>h &&f>i &&f>j)
+
+	     System.out.println(f);
+
+	     else if (g>a && g>b && g>c &&g>d &&g>e &&g>f &&g>h &&g>i &&g>j)
+
+	     System.out.println(g);
+
+	     else if (h>a && h>b && h>c &&h>d &&h>e &&h>f &&h>g &&h>i &&h>j)
+
+	     System.out.println(h);
+
+	     else if (i>a && i>b && i>c &&i>d &&i>e &&i>f &&i>g &&i>h &&i>j)
+
+	     System.out.println(i);
+
+	     else
+
+	       System.out.println(j);
+
+	}
+}
